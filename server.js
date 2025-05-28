78653ff64977d4a3c5893c0c5feca41746441b62
Naresh-Anchara <nareshanchara123@gmail.com>
2025-05-28 18:22:22 +0530

    Initial commit

--- server.js
+++ server.js
@@ -15,7 +15,22 @@ const port =  5000;
 
 // Middleware
 //const cors = require('cors');
-app.use(cors());
+const allowedOrigins = ["http://localhost:3000"]; 
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
